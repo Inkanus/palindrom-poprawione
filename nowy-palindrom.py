b6883435ddb91364bd82d31f7a73fc38be56b46a
Inkanus <73430904+Inkanus@users.noreply.github.com>
2020-11-25 17:24:08 +0100

    Add files via upload

--- nowy-palindrom.py
+++ nowy-palindrom.py
@@ -1,13 +1,22 @@
-p = "Kajak kajak"
-p = p.lower()
-
-def is_palindrome(i):
-   if len(i) <= 1:
-       return True
-   else:
-       if i[0] != i[len(i)-1]:
-           return False
-       else:
-           return is_palindrome(i[1:len(i)-1])
-print(is_palindrome(p))
-
+def Palindrome(s): 
+    l, h = 0, len(s) - 1
+    s = s.lower() 
+  
+    while (l <= h): 
+        if (not(s[l] >= 'a' and s[l] <= 'z')): 
+            l += 1
+        elif (not(s[h] >= 'a' and s[h] <= 'z')): 
+            h -= 1
+        elif (s[l] == s[h]): 
+            l += 1
+            h -= 1
+        else: 
+            return False
+    return True
+    
+s = "Madam, I'm Adam."
+if (Palindrome(s)): 
+    print("True")
+else: 
+    print("False")
+    
